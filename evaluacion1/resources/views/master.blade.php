6fccb161f4d548c77f2b55c7d5210b7faa11ac6c
Jose Contreras <jose.contreras.venegas@ciisa.cl>
2022-07-20 20:49:21 -0400

    primer commit

---
+++ evaluacion1/resources/views/master.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blackat Store</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <div class= container>
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Blackat Store</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Registrar Producto</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Consultar Producto</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Eliminar Producto</a>
+        </li>
+      </ul>
+      <ul ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="https://www.youtube.com/watch?v=5p5d1vflc_g">Tutorial como Registrar, Consultar, Eliminar productos</a>
+      </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+  </div>
+  <div class="container">
+        <!-- Footer -->
+        <footer class="footer" position="fixed">
+            <p class="footer__p">
+                &copy; Copyright 2020 &copy; <a href="#">Pingui Studio</a>
+            </p>
+        </footer>
+    </div>
+</html>
